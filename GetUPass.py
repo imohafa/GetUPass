6f06bcc0804ac8030e085d51c50bc2ebb36612f1
Moodix <moodx94@hotmail.com>
2022-07-09 14:38:41 +0300

    Update GetUPass.py

--- GetUPass.py
+++ GetUPass.py
@@ -1,7 +1,7 @@
 import getpass
 import pwd
 
-print("     GetUPass  \n Twitter: @moodix94 \n Github: @moodix94 \n")
+print("     GetUPass  \n Twitter: @moodix0 \n Github: @moodix94 \n")
 
 
 def UserName():
